bda3f9eab9f8c622b34cc8f4285a37f68e58feef
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-03-22 21:59:48 -0300

    user model

---
+++ app-front/src/app/classes/user.model.ts
@@ -0,0 +1,19 @@
+export class User {
+  name: string;
+  email: string;
+  password: string;
+  age: number;
+  weight: number;
+  createdAt: Date;
+  bornAt: Date;
+ 
+  constructor(partial: Partial<User> = {}) {
+    this.name      = partial.name      ?? '';
+    this.email     = partial.email     ?? '';
+    this.password  = partial.password  ?? '';
+    this.age       = partial.age       ?? 0;
+    this.weight    = partial.weight    ?? 0;
+    this.createdAt = partial.createdAt ?? new Date();
+    this.bornAt    = partial.bornAt    ?? new Date();
+  }
+}
