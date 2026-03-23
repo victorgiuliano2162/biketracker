68cd4dec5662d01a2d736da41ead9b002ec5002a
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-03-22 22:03:26 -0300

    enum for blood type

--- app/src/main/java/br/com/biketracker/app/entities/enums/TipoSanguineo.java
+++ app/src/main/java/br/com/biketracker/app/entities/enums/TipoSanguineo.java
@@ -1,4 +1,30 @@
 package br.com.biketracker.app.entities.enums;
 
-public class TipoSanguineo {
+import lombok.Getter;
+
+@Getter
+public enum TipoSanguineo {
+    A_POSITIVO("A+"),
+    A_NEGATIVO("A-"),
+    B_POSITIVO("B+"),
+    B_NEGATIVO("B-"),
+    AB_POSITIVO("AB+"),
+    AB_NEGATIVO("AB-"),
+    O_POSITIVO("O+"),
+    O_NEGATIVO("O-");
+
+    private final String descricao;
+
+    TipoSanguineo(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return this.descricao;
+    }
 }
