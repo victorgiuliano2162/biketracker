ebaf749cadf8bbc6f09b36063f51996b8f59a3b6
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-03-24 20:25:34 -0300

    config to allow jwt properties class

--- app/src/main/java/br/com/biketracker/app/AppApplication.java
+++ app/src/main/java/br/com/biketracker/app/AppApplication.java
@@ -1,9 +1,12 @@
 package br.com.biketracker.app;
 
+import br.com.biketracker.app.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class AppApplication {
 
     public static void main(String[] args) {
