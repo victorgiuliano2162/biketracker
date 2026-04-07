6f034f4da949aa63f5d6dea82ba1c31d45f6884b
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-04-06 23:00:48 -0300

    added service annotation for spring

--- app/src/main/java/br/com/biketracker/app/services/UserDetailsServiceImpl.java
+++ app/src/main/java/br/com/biketracker/app/services/UserDetailsServiceImpl.java
@@ -5,7 +5,10 @@ import br.com.biketracker.app.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
