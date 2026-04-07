4440884df2b1b4398411f1c5b7afb5a583a4333d
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-04-06 22:56:26 -0300

    spinner component

--- app-front/src/app/app.component.ts
+++ app-front/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { TopBarComponent } from "./components/top-bar/top-bar.component";
+import { SpinneroverlayComponent } from "./components/spinneroverlay/spinneroverlay.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, FooterComponent, TopBarComponent],
+  imports: [RouterOutlet, FooterComponent, TopBarComponent, SpinneroverlayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
