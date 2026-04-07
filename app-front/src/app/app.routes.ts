e4c04dc27242a425904753caf9e4bf864d74a1fa
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-04-06 22:57:09 -0300

    routes refactor

--- app-front/src/app/app.routes.ts
+++ app-front/src/app/app.routes.ts
@@ -1,19 +1,32 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
+import { authGuard, publicGuard } from './guards/Auth.guard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: '/login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        component: LoginPageComponent,
-    },
-    {
-        path: 'subscribe',
-        component: CreateUserComponent
-    },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login-page/login-page.component').then((m) => m.LoginPageComponent),
+    canActivate: [publicGuard],
+  },
+  {
+    path: 'subscribe',
+    loadComponent: () =>
+      import('./components/create-user/create-user.component').then(
+        (m) => m.CreateUserComponent,
+      ),
+    canActivate: [publicGuard],
+  },
+
+  // redireciona para login se não autenticado
+  {
+    path: 'home',
+    loadComponent: () =>
+        import('./components/home/home.component').then((m) => m.HomeComponent),
+    canActivate: [authGuard],
+  },
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
 ];
