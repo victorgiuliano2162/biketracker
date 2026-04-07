1beb275e9781f2ace30955a1d6c817b69c672db4
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-04-07 00:07:09 -0300

    remade goals entity

--- app/src/main/java/br/com/biketracker/app/repositories/GoalRepository.java
+++ app/src/main/java/br/com/biketracker/app/repositories/GoalRepository.java
@@ -4,7 +4,10 @@ import br.com.biketracker.app.entities.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
 
+    List<Goal> findByUserIdOrderByCreatedAtDesc(String userId);
 }
