b6baab4e2a1ea370a8c64ab0add328eb91ca77a1
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-04-06 22:59:57 -0300

    added beans validations

--- app/src/main/java/br/com/biketracker/app/entities/User.java
+++ app/src/main/java/br/com/biketracker/app/entities/User.java
@@ -2,6 +2,7 @@ package br.com.biketracker.app.entities;
 
 import br.com.biketracker.app.entities.enums.TipoSanguineo;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,13 +21,32 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
     private String name;
+
+    @NotBlank(message = "E-mail é obrigatório")
+    @Email(message = "E-mail inválido")
     private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
     private String password;
+
+    @Min(value = 1, message = "Idade mínima é 1 ano")
+    @Max(value = 120, message = "Idade máxima é 120 anos")
     private int age;
+
+    @DecimalMin(value = "1.0", message = "Peso mínimo é 1kg")
+    @DecimalMax(value = "500.0", message = "Peso máximo é 500kg")
     private double weight;
+
     private LocalDateTime createdAt;
+
+    @NotNull(message = "Data de nascimento é obrigatória")
+    @PastOrPresent(message = "Data de nascimento não pode ser futura")
     private LocalDateTime bornAt;
+
     private TipoSanguineo tipoSanguineo;
 
     @OneToMany(cascade = CascadeType.ALL)
