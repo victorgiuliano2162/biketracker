a5d03acad7dd6b4c49770f69d29e6e239ce558bb
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-04-08 14:52:15 -0300

    new method to get user

--- app/src/main/java/br/com/biketracker/app/controllers/UserController.java
+++ app/src/main/java/br/com/biketracker/app/controllers/UserController.java
@@ -2,15 +2,17 @@ package br.com.biketracker.app.controllers;
 
 
 import br.com.biketracker.app.entities.User;
-import br.com.biketracker.app.entities.dtos.authDto.UserRequest;
-import br.com.biketracker.app.entities.dtos.authDto.UserResponse;
+import br.com.biketracker.app.entities.dtos.UserRequest;
+import br.com.biketracker.app.entities.dtos.UserResponse;
 import br.com.biketracker.app.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -40,4 +42,13 @@ public class UserController {
                 .body(UserResponse.from(userService.save(user)));
 
     }
+
+    //TODO allow only authenticated requests to acesses this method
+    @GetMapping
+    public ResponseEntity<UserResponse> getUser(@RequestParam String id) {
+        User user = userService.findById(id);
+        return user != null ? ResponseEntity.ok(UserResponse.from(user)) : ResponseEntity.notFound().build();
+    }
+
+
 }
