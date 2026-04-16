2374b3bb0858b365147d0de868aa1db55a1a8063
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-04-15 23:44:39 -0300

    Update DTO pagenation

--- app/src/main/java/br/com/biketracker/app/AppApplication.java
+++ app/src/main/java/br/com/biketracker/app/AppApplication.java
@@ -4,9 +4,13 @@ import br.com.biketracker.app.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties.class)
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class AppApplication {
 
     public static void main(String[] args) {
