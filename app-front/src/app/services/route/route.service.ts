c8d963cf77fea8b47e0995fbc17a8d9bfc957955
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-04-15 23:48:52 -0300

    Create route service

---
+++ app-front/src/app/services/route/route.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+
+export interface TrackPoint {
+  latitude: number;
+  longitude: number;
+  altitudeInMeters: number;
+  recordedAt: string; // ISO 8601
+}
+
+export interface CreateRouteRequest {
+  distanceInKm: number;
+  elevationInMeters: number;
+  startTime: string;
+  endTime: string;
+  startCity: string;
+  country: string;
+  isPublic: boolean;
+  trackPoints: TrackPoint[];
+  name: String;
+}
+
+export interface RouteResponse {
+  id: number;
+  name: string,
+  distanceInKm: number;
+  elevationInMeters: number;
+  startTime: string;
+  endTime: string;
+  startCity: string;
+  country: string;
+  activityTimeInSeconds: number;
+  isPublic: boolean;
+}
+
+export interface RouteReplayResponse {
+  routeId: string;
+  points: TrackPoint[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteService {
+
+  private readonly base = '/api/routes';
+
+  constructor(private http: HttpClient) { }
+
+  listMine(page = 0, size = 10): Observable<{ content: RouteResponse[]; totalElements: number }> {
+    return this.http.get<{ content: RouteResponse[]; totalElements: number }>(
+    `${this.base}/my`, { params: { page, size } }
+    );
+  }
+
+  save(request: CreateRouteRequest): Observable<RouteResponse> {
+    return this.http.post<RouteResponse>(this.base, request);
+  }
+
+  getReplay(routeId: string): Observable<RouteReplayResponse> {
+  return this.http.get<RouteReplayResponse>(`${this.base}/my/${routeId}/replay`);
+}
+
+}
