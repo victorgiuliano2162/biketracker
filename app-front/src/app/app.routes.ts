add9789df5b4c8d2977a164eec9728e020948251
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-05-30 17:46:12 -0300

    Fix formating style

--- app-front/src/app/app.routes.ts
+++ app-front/src/app/app.routes.ts
@@ -1,7 +1,4 @@
-import { MapComponent } from './components/map/map.component';
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { CreateUserComponent } from './components/create-user/create-user.component';
 import { authGuard, publicGuard } from './guards/Auth.guard';
 
 export const routes: Routes = [
@@ -14,36 +11,45 @@ export const routes: Routes = [
   {
     path: 'subscribe',
     loadComponent: () =>
-      import('./components/create-user/create-user.component').then(
-        (m) => m.CreateUserComponent,
-      ),
+      import('./components/create-user/create-user.component').then((m) => m.CreateUserComponent),
     canActivate: [publicGuard],
   },
-
-  // redireciona para login se não autenticado
   {
     path: 'home',
     loadComponent: () =>
-        import('./components/home/home.component').then((m) => m.HomeComponent),
+      import('./components/home/home.component').then((m) => m.HomeComponent),
     canActivate: [authGuard],
   },
   {
     path: 'map',
-    loadComponent: () => import('./components/map/map.component').then((m) => m.MapComponent), canActivate: [authGuard],
+    loadComponent: () =>
+      import('./components/map/map.component').then((m) => m.MapComponent),
+    canActivate: [authGuard],
   },
-   {
+  {
     path: 'routes',
-    loadComponent: () => import('./components/my-routes/my-routes.component').then((m) => m.MyRoutesComponent), canActivate: [authGuard],
+    loadComponent: () =>
+      import('./components/my-routes/my-routes.component').then((m) => m.MyRoutesComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'routes/:id',
+    loadComponent: () =>
+      import('./components/route-detail/route-detail.component').then((m) => m.RouteDetailComponent),
+    canActivate: [authGuard],
   },
   {
     path: 'goals',
-    loadComponent: () => import('./components/goal-list/goal-list.component').then((m) => m.GoalListComponent), canActivate: [authGuard]
+    loadComponent: () =>
+      import('./components/goal-list/goal-list.component').then((m) => m.GoalListComponent),
+    canActivate: [authGuard],
   },
   {
     path: 'goal',
-    loadComponent: () => import('./components/goal/goal.component').then((m) => m.GoalComponent), canActivate: [authGuard]
+    loadComponent: () =>
+      import('./components/goal/goal.component').then((m) => m.GoalComponent),
+    canActivate: [authGuard],
   },
-  {},
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' },
