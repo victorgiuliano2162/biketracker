9b3fc55e5621ebdb93cf7669134be94ac7129ba9
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-06-10 17:39:16 -0300

    (create): reset password endpoints

--- app-front/src/app/services/auth/auth.service.ts
+++ app-front/src/app/services/auth/auth.service.ts
@@ -2,55 +2,57 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs';
-
+import { Observable } from 'rxjs/internal/Observable';
 
 export interface LoginRequest {
   email: string;
   password: string;
 }
- 
+
 export interface LoginResponse {
   accessToken: string;
   refreshToken: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private readonly apiUrl = '/api/auth';
 
   readonly isAuthenticated = signal(this.hasValidToken());
 
-  constructor(private http: HttpClient, private router: Router) {   }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
 
-  
   login(request: LoginRequest) {
     return this.http.post<LoginResponse>(`${this.apiUrl}/login`, request).pipe(
-      tap(response => {
+      tap((response) => {
         localStorage.setItem('accessToken', response.accessToken);
         localStorage.setItem('refreshToken', response.refreshToken);
         this.isAuthenticated.set(true);
-        console.log("Acces Token:", response.accessToken);
-      })
+        console.log('Acces Token:', response.accessToken);
+      }),
     );
   }
 
-
-    refresh() {
+  refresh() {
     const refreshToken = localStorage.getItem('refreshToken');
     if (!refreshToken) {
       this.logout();
       return;
     }
 
-      return this.http.post<LoginResponse>(`${this.apiUrl}/refresh`, { refreshToken }).pipe(
-      tap(response => {
-        localStorage.setItem('accessToken', response.accessToken);
-        localStorage.setItem('refreshToken', response.refreshToken);
-      })
-    );
+    return this.http
+      .post<LoginResponse>(`${this.apiUrl}/refresh`, { refreshToken })
+      .pipe(
+        tap((response) => {
+          localStorage.setItem('accessToken', response.accessToken);
+          localStorage.setItem('refreshToken', response.refreshToken);
+        }),
+      );
   }
 
   logout(): void {
@@ -59,15 +61,15 @@ export class AuthService {
     this.isAuthenticated.set(false);
     this.router.navigate(['/login']);
   }
- 
+
   getAccessToken(): string | null {
     return localStorage.getItem('accessToken');
   }
- 
+
   private hasValidToken(): boolean {
     const token = localStorage.getItem('accessToken');
     if (!token) return false;
- 
+
     try {
       // Decodifica o payload do JWT sem biblioteca externa
       const payload = JSON.parse(atob(token.split('.')[1]));
@@ -78,4 +80,15 @@ export class AuthService {
     }
   }
 
+  //To retrive password
+  forgotPassword(email: string): Observable<void> {
+    return this.http.post<void>('/api/auth/forgot-password', { email });
+  }
+
+  resetPassword(token: string, newPassword: string): Observable<void> {
+    return this.http.post<void>('/api/auth/reset-password', {
+      token,
+      newPassword,
+    });
+  }
 }
